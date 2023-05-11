149bab80f48b0ce064705cfc988f8bf73f2b9c62
seoyamin <seoyamin@ewhain.net>
2023-05-11 13:06:21 +0900

    feat : RDS Config

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import torch
 from flask import Flask, request
 from PIL import Image
 
+import config
+from db_connect import db
+
 from flask_jsonpify import jsonpify 
 import json
 
@@ -17,18 +20,12 @@ firebase_admin.initialize_app(cred)
 app = Flask(__name__)
 models = {}
 
-
 @app.route("/model/search/<model>", methods=['POST'])
 def predict(model):
     if request.method != 'POST':
         return
 
     if request.files.get('image'):
-        # Method 1
-        # with request.files["image"] as f:
-        #     im = Image.open(io.BytesIO(f.read()))
-
-        # Method 2
         im_file = request.files['image']
         im_bytes = im_file.read()
         im = Image.open(io.BytesIO(im_bytes))
@@ -47,7 +44,7 @@ def predict(model):
         return json_data
 
 
-@app.route("/video/<model>", methods=['POST'])
+@app.route("/report/<model>", methods=['POST'])
 def analyze(model):
     if request.method != 'POST':
         return
@@ -55,11 +52,19 @@ def analyze(model):
     params = request.get_json()
     if params:
         video_url = params['url']
+        created_at = params['created_at']
         
         if model in models:
             results = models[model](video_url, size = 1920)
             print(results.pandas().xyxy[0][0].to_json(orient='records'))
             return "hello"
+
+
+def create_app():
+    app.config.from_object(config)
+    db.init_app(app)
+    
+    return app
 
 
 if __name__ == '__main__':
@@ -71,4 +76,5 @@ if __name__ == '__main__':
     for m in opt.model:
         models[m] = torch.hub.load('ultralytics/yolov5', 'custom', './yolov5/best.pt', force_reload=True, skip_validation=True)
 
-    app.run(host='0.0.0.0', port=opt.port)  # debug=True causes Restarting with stat
+    create_app().run()
+    #app.run(host='0.0.0.0', port=opt.port)  # debug=True causes Restarting with stat
